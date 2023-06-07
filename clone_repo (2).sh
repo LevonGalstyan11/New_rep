53dd84b16d90d5cd0c6c0684f0dbec5650ed1607
Levon Galstyan <levongalstyan98@gmail.com>
2023-06-07 15:08:16 +0400

    my first commit ever

---
+++ clone_repo (2).sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+git clone https://LevonGalstyan11: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@github.com/$1
+
+# $1 = USERNAME
+# $2 = PERSONAL_ACCESS_TOKEN
+# $3 = REPOSITORY_NAME
